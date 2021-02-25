46d0c5fb15b23a418e3112f495dbbf9130a5bc93
ClaraBing <bingbinl@cs.cmu.edu>
2021-02-24 22:42:33 -0500

    add option for normalized or not

--- main.py
+++ main.py
@@ -32,14 +32,21 @@ if __name__ == '__main__':
     parser.add_argument('--clamp', type=float, default=1e-2,
                         help='Clipping gradients for WGAN.')
     # NOTE: added by BB
+    parser.add_argument('--normalize-data', type=int, default=1)
+    parser.add_argument('--use-l2-loss', type=int, default=0,
+                        help="Whether to use latent / image l2 loss.")
+    parser.add_argument('--l2-loss-weight', type=float, default=3.0,
+                        help="Weight for latent / image l2 loss.")
     parser.add_argument('--save-path', type=str, default='',
                         help="File path for ckpt.")
     parser.add_argument('--save-token', type=str, default='',
                         help="Suffix for save_path")
     #parsing arguments.
     args = parser.parse_args()
-    args.save_path = 'BiGAN_lr{}_wd1e-6_bt{}_dim{}_W{}_epoch{}{}.pt'.format(
-      args.lr_adam, args.batch_size, args.latent_dim, 1 if args.wasserstein else 0, args.num_epochs,
+    args.save_path = 'BiGAN_lr{}_wd1e-6_bt{}_dim{}_W{}_{}epoch{}{}.pt'.format(
+      args.lr_adam, args.batch_size, args.latent_dim, 1 if args.wasserstein else 0,
+      'l2{}'.format(args.l2_loss_weight) if args.use_l2_loss else '',
+      args.num_epochs,
       '_'+args.save_token if args.save_token else '')
     if USE_WANDB:
       wandb.init(project='visualize', name=args.save_path, config=args)
--- preprocess.py
+++ preprocess.py
@@ -7,13 +7,19 @@ import torchvision.transforms as transforms
 
 def get_cifar10(args, data_dir='./data/cifar/'):
     """Returning cifar dataloder."""
-    transform = transforms.Compose([
-        transforms.Resize(32), #3x32x32 images.
-        transforms.ToTensor(),
-        transforms.Normalize(
-            mean = [0.4913997551666284, 0.48215855929893703, 0.4465309133731618],
-            std = [0.24703225141799082, 0.24348516474564, 0.26158783926049628]),
-        ])
+    if args.normalize_data:
+        transform = transforms.Compose([
+            transforms.Resize(32), #3x32x32 images.
+            transforms.ToTensor(),
+            transforms.Normalize(
+                mean = [0.4913997551666284, 0.48215855929893703, 0.4465309133731618],
+                std = [0.24703225141799082, 0.24348516474564, 0.26158783926049628]),
+            ])
+    else:
+        transform = transforms.Compose([
+            transforms.Resize(32), #3x32x32 images.
+            transforms.ToTensor(),
+            ])
     data = datasets.CIFAR10(root=data_dir, train=True, download=True, transform=transform)
     dataloader = DataLoader(data, batch_size=args.batch_size, shuffle=True)
     return dataloader
