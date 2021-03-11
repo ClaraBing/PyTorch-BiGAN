21b667e83c2591429f5b255d6dc34ad6e436b140
ClaraBing <bingbinl@cs.cmu.edu>
2021-03-11 11:21:44 -0500

    skip updates for pixels with grad=nan

--- vis_max_act.py
+++ vis_max_act.py
@@ -16,7 +16,6 @@ from torch.autograd import Variable
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
-import matplotlib.pyplot as plt
 import importlib
 import cv2
 # from scipy.ndimage.filters import gaussian_filter1d
@@ -38,8 +37,8 @@ IMG_MEAN = torch.tensor([0.5, 0.5, 0.5])
 IMG_STD = torch.tensor([0.5, 0.5, 0.5])
 
 
-def get_encoder(latent_dim, fckpt=''):
-  E = Encoder(latent_dim)
+def get_encoder(latent_dim, fckpt='', ker_size=11):
+  E = Encoder(z_dim=latent_dim, first_filter_size=ker_size)
   if fckpt and os.path.exists(fckpt):
     ckpt = torch.load(fckpt)
     loaded_sd = ckpt['E']
@@ -62,6 +61,8 @@ def get_encoder(latent_dim, fckpt=''):
       curr_params.update(updated_params)
       E.load_state_dict(curr_params)
   return E.to(device)
+
+scale = 1
 
 def get_max_act(E, img_size, latent_dim, coord_idx,
                 eta=0.01, subfolder='', img_token='', img_norm=1):
@@ -91,7 +92,7 @@ def get_max_act(E, img_size, latent_dim, coord_idx,
     img = img + img.grad * eta
     img = img.detach()
     mean, std = img.mean(), img.std()
-    bdd = min(4 * std, 0.5)
+    bdd = min(scale * std, 0.5)
     img -= img.mean()
     img[img>bdd] = bdd
     img[img<-bdd] = -bdd
@@ -148,9 +149,10 @@ def yosinski(coord_idx, model, **kwargs):
     if subfolder:
       img_dir = os.path.join(img_dir, subfolder)
       os.makedirs(img_dir, exist_ok=1)
-    if ret_layer != -1:
-      img_dir = os.path.join(img_dir, 'layer{}'.format(ret_layer))
-      os.makedirs(img_dir, exist_ok=1)
+
+    img_dir = os.path.join(img_dir, 'layer{}'.format(ret_layer))
+    os.makedirs(img_dir, exist_ok=1)
+    os.makedirs(os.path.join(img_dir, '0_final'), exist_ok=1)
 
     def jitter(X, ox, oy):
         """
@@ -183,6 +185,8 @@ def yosinski(coord_idx, model, **kwargs):
         scores = model(img, ret_layer=ret_layer)
         loss = -scores[0, coord_idx] + l2_reg * (img * img).sum()
         loss.backward()
+        # ignore the pixels that is blown up.
+        img.grad.data[torch.isnan(img.grad.data)] = 0
         img.data.add_(-learning_rate, img.grad.data)
         img.grad.data.zero_()
         
@@ -190,34 +194,54 @@ def yosinski(coord_idx, model, **kwargs):
         img.data.copy_(jitter(img.data, -ox, -oy))
 
         # As regularizer, clamp and periodically blur the image
-        for c in range(3):
-            # lo = float(-SQUEEZENET_MEAN[c] / SQUEEZENET_STD[c])
-            # hi = float((1.0 - SQUEEZENET_MEAN[c]) / SQUEEZENET_STD[c])
-            lo = float(-CIFAR_MEAN[c] / CIFAR_STD[c])
-            hi = float((1.0 - CIFAR_MEAN[c]) / CIFAR_STD[c])
-            img.data[:, c].clamp_(min=lo, max=hi)
+        for ch in range(3):
+            lo = float(-SQUEEZENET_MEAN[ch] / SQUEEZENET_STD[ch])
+            hi = float((1.0 - SQUEEZENET_MEAN[ch]) / SQUEEZENET_STD[ch])
+            # lo2 = float(-CIFAR_MEAN[ch] / CIFAR_STD[ch])
+            # hi2 = float((1.0 - CIFAR_MEAN[ch]) / CIFAR_STD[ch])
+            img.data[:, ch].clamp_(min=lo, max=hi)
         if t % blur_every == 0:
             # sigma = 0.5
             sigma = 0.1
             blur_image(img.data, sigma)
 
-        # pdb.set_trace()
-        
         # Periodically show the image
         if t == 0 or (t + 1) % show_every == 0 or t == num_iterations - 1:
             img_vis = img.data.clone().cpu()
             if bdd_pixels:
-              img_vis = bound_pixels(img_vis)
+              img_vis = bound_pixels(img_vis, scale)
             if upto != -1:
               img_vis = img_vis[:, :, :upto, :upto]
             image = deprocess(img_vis)
-            plt.imshow(image)
-            plt.savefig(os.path.join(img_dir, 'c{}_{}iter{}.png'.format(coord_idx, img_token, t+1)))
+            if t == num_iterations - 1:
+              fimg = os.path.join(img_dir, '0_final', 'c{}_{}iter{}.png'.format(coord_idx, img_token, t+1))
+            else:
+              fimg = os.path.join(img_dir, 'c{}_{}iter{}.png'.format(coord_idx, img_token, t+1))
+            image = np.array(image)
+            if image.max() == 0:
+              print("Dead pixels.")
+              pdb.set_trace()
+            cv2.imwrite(fimg, image)
 
     return deprocess(img.data.cpu())
 
 if __name__ == '__main__':
-  latent_dim = 256
+  image_size = 32
+
+  if 0:
+    # fckpt = 'ckpts/BiGAN_lr0.0003_wd1e-6_bt128_dim256_W0_epoch800.pt'
+    # fckpt = 'ckpts/BiGAN_lr0.0001_wd1e-6_bt128_dim256_W0_l23.0_epoch200.pt'
+    fckpt = 'ckpts/BiGAN_lr0.0001_wd1e-6_bt128_dim256_W0_l23.0_epoch800_cont_tmp_e200.pt'
+  if 0:
+    fckpt = 'ckpts/BiGAN_lr0.0001_wd1e-6_bt32_dim128_k11_W0_l23.0_epoch800__bn2conv_tmp_e750.pt'
+    subfolder = 'e800_k11'
+  if 1:
+    # Mar04: freeze G&D (750 epochs), train E for 350 more epochs.
+    ker_size = 11
+    latent_dim = 128
+    fckpt = 'ckpts/BiGAN_cifar_lr0.0001_wd1e-6_bt32_dim128_k11_W0_l23.0_epoch800_freezeGD__bn2conv_tmp_e350.pt'
+    subfolder = 'e750_freezeGD_e350_k{}'.format(ker_size)
+
   hidden_dims = {
     1: 32,
     2: 64,
@@ -231,31 +255,30 @@ if __name__ == '__main__':
     1: 18,
     3: 23,
   }
-  image_size = 32
-  # fckpt = 'ckpts/BiGAN_lr0.0003_wd1e-6_bt128_dim256_W0_epoch800.pt'
-  # fckpt = 'ckpts/BiGAN_lr0.0001_wd1e-6_bt128_dim256_W0_l23.0_epoch200.pt'
-  fckpt = 'ckpts/BiGAN_lr0.0001_wd1e-6_bt128_dim256_W0_l23.0_epoch800_cont_tmp_e200.pt'
-  fckpt = 'ckpts/BiGAN_lr0.0001_wd1e-6_bt32_dim128_k11_W0_l23.0_epoch800__bn2conv_tmp_e750.pt'
-  E = get_encoder(hidden_dims[-1], fckpt)
+  E = get_encoder(hidden_dims[-1], fckpt, ker_size=ker_size)
 
   img_size = [3, IMG_SIZE, IMG_SIZE]
   coord_idx = 0
-  subfolder = 'pretrained'
+  # subfolder = 'pretrained'
   # img_token = 'random'
   img_token = ''
   # get_max_act(E, img_size, latent_dim, coord_idx, subfolder=subfolder, img_token=img_token)
 
   # subfolder = 'e200_unnorm_sigma0.1_cont200'
-  subfolder = 'e800_k11'
   # img_token = 'scaled_'
-  ret_layer = 3
+  ret_layer = -1
   bdd_pixels = 1
-  upto = uptos[ret_layer]
-  lr = 1
+  if ret_layer in uptos:
+    upto = uptos[ret_layer]
+  else:
+    upto = -1
   l2_reg = 1e-2
-  img_token = '1std_lr{}_L2{}_'.format(lr, l2_reg)
-  num_iterations = 500
-  for coord_idx in range(hidden_dims[ret_layer]):
-    yosinski(coord_idx, E, subfolder=subfolder, ret_layer=ret_layer,
-      learning_rate=lr, l2_reg=l2_reg, num_iterations=num_iterations,
-      upto=upto, img_token=img_token, bdd_pixels=bdd_pixels)
+  num_iterations = 1000
+  for lr in [10, 3, 1, 20]:
+    img_token = '{}std_lr{}_reg{}_'.format(scale, lr, l2_reg)
+    for coord_idx in range(hidden_dims[ret_layer]):
+      yosinski(coord_idx, E, subfolder=subfolder, ret_layer=ret_layer,
+        learning_rate=lr, l2_reg=l2_reg, num_iterations=num_iterations,
+        upto=upto, img_token=img_token, bdd_pixels=bdd_pixels)
+      
+      break
